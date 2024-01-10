06d2814c3800c5012f0e3b543b37f22719173d9c
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-01-09 19:38:14 -0500

    fetching issues

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default() => {
+import axios from "axios"
+
+const LandindgPage = ({currentUser}) => {
+  console.log(currentUser)
+
   return <h1>Landing Page</h1>
 }
+
+LandindgPage.getIntialProps = async () => {
+  const response = await axios.get('/api/users/currentuser')
+
+  return response.data
+}
+
+export default LandindgPage
