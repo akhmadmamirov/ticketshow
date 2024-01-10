bf1d757c742b5127762729f4bbacb1fbebfeaa42
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-01-10 10:34:51 -0500

    seetting reqs from browser and ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,31 @@
-import axios from "axios"
+import axios from 'axios';
 
-const LandindgPage = ({currentUser}) => {
-  console.log(currentUser)
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  // axios.get('/api/users/currentuser');
 
-  return <h1>Landing Page</h1>
-}
+  return <h1>Landing Page</h1>;
+};
 
-LandindgPage.getIntialProps = async () => {
-  const response = await axios.get('/api/users/currentuser')
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+      )
+    return data
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser')
+    return data
+  }
+  return {};
+};
 
-  return response.data
-}
-
-export default LandindgPage
+export default LandingPage;
