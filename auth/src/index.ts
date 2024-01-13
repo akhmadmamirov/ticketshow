993df26e059035099e658e6e641ceb1d3ae732b0
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-01-13 13:11:07 -0500

    fixing mongodb connection err

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
-
-    if (!process.env.JWT_KEY){
-      throw new Error('JWT_Key must be defined')
-    }
-
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
 
     console.log('Connected to MongoDb');
