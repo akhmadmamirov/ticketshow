f5be6c1dffe5d63459871553450f4b7475f63a88
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-01-14 10:39:02 -0500

    added tests for new tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@rallycoding/common';
+import { errorHandler, NotFoundError, currentUser } from 'akhmadillobekcommon';
+import { createTicketRouter } from "./routes/new"
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,10 +15,14 @@ app.use(
   })
 );
 
+app.use(currentUser)
+app.use(createTicketRouter)
+
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
 app.use(errorHandler);
+
 
 export { app };
