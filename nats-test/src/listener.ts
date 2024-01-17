fb859c59893416492235be3ffbcc0c0daf39165f
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-01-17 09:12:34 -0500

    adding publisher and listener

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,21 @@
+import nats, { Message } from 'node-nats-streaming'
+
+console.log()
+
+const stan = nats.connect("ticketing", "123", {
+  url: "https://localhost:4222"
+})
+
+stan.on('connect', () => {
+  console.log("Listener connected to NATS")
+
+  const subscription = stan.subscribe('ticket:created')
+
+  subscription.on('message', (msg : Message) => {
+    const data = msg.getData()
+
+    if (typeof data === 'string') {
+      console.log(`Received an event #${msg.getSequence()}, with data: ${data}`)
+    }
+  })
+})
