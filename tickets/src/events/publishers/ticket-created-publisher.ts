febcf63de62fb23885fd18fdd66448918733ab38
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-01-24 09:53:55 -0500

    adding new route

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -1,5 +1,5 @@
-import { Publisher, Subjects, TicketCreatedEvent } from "akhmadillobekcommon";
+import { Publisher, Subjects, TicketCreatedEvent } from 'akhmadillobekcommon';
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-   subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  readonly subject = Subjects.TicketCreated;
 }
