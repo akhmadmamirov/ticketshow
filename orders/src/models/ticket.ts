2253a6fbfa26d94626974e9344f47d0a99475674
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-01-29 20:46:42 -0500

    adding tests for new orders

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,58 +1,62 @@
-import mongoose, { Mongoose, mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import { Order, OrderStatus } from './order';
 
-
 interface TicketAttrs {
-  title: string, 
-  price: number
+  title: string;
+  price: number;
 }
 
 export interface TicketDoc extends mongoose.Document {
-  title: string, 
-  price: number,
-  isReserved(): Promise<boolean> 
+  title: string;
+  price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
-  build(attrs: TicketAttrs): TicketDoc
+  build(attrs: TicketAttrs): TicketDoc;
 }
 
-const ticketSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
   },
-  price: {
-    type: Number, 
-    min: 0
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
   }
-}, {
-  toJSON : {
-    transform(doc, ret) {
-      ret.id = ret._id
-      delete ret._id
-    }
-  }
-})
+);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket({attrs})
-}
+  return new Ticket(attrs);
+};
 ticketSchema.methods.isReserved = async function () {
-  //this === ticket document that we just called 'isReserved' on
+  // this === the ticket document that we just called 'isReserved' on
   const existingOrder = await Order.findOne({
-    ticket: this.ticket,
+    ticket: this,
     status: {
-      $in : [
+      $in: [
         OrderStatus.Created,
         OrderStatus.AwaitingPayment,
-        OrderStatus.Complete
-      ]
-    }
-  })
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
   return !!existingOrder;
-}
+};
 
-const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema)
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
-export { Ticket }
+export { Ticket };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -28,38 +28,36 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const { ticketId } = req.body
+    const { ticketId } = req.body;
 
-    //Find the ticket user is trying to order
-    const ticket = await Ticket.findById(ticketId)
-
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
     if (!ticket) {
       throw new NotFoundError();
     }
-    //Make sure this ticket is not already reserved:
-    const isReserved = await ticket.isReserved()
 
+    // Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
     if (isReserved) {
-      throw new BadRequestError('Ticket is already reserved')
+      throw new BadRequestError('Ticket is already reserved');
     }
 
-    //Calculate an expiration date for the order
-    const expiration = new Date()
-    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
-    //Build the order and save it to the database
+    // Build the order and save it to the database
     const order = Order.build({
       userId: req.currentUser!.id,
       status: OrderStatus.Created,
       expiresAt: expiration,
-      ticket
-    })
-    await order.save()
+      ticket,
+    });
+    await order.save();
 
-    //Publish an event saying that the order was created
+    // Publish an event saying that an order was created
 
-    res.status(201).send(order)
+    res.status(201).send(order);
   }
 );
-
 export { router as newOrderRouter };
