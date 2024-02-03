69d9a5d4e48cfb5770082f898f51d8bd019eaf25
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-02-02 21:58:46 -0500

    test

---
+++ orders/src/events/listeners/__test__/ticket-created-event.test.ts
@@ -0,0 +1,26 @@
+const setup = async() => {
+  //create an instance of the listener
+
+  //creates fake data event
+
+  //creates a fake message object
+}
+
+it('creates and saves a ticket', async () => {
+
+  //call on message function with the data object + message object
+
+  //write assertions to make sure ticket was created!
+
+
+})
+
+it('acks the message', async () => {
+  //acknowledging message
+
+  //call on message function with the data object + message object
+
+  //write assertions to make sure ticket was created!
+
+  //write assertions to make sure ack function is called
+})
