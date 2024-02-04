f0ce5e7c3e6f24427443199d8b94cb8b857873f9
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-02-04 15:01:22 -0500

    rejecting updates if the ticket is reserved

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 import { natsWrapper } from '../../nats-wrapper';
+import { Ticket } from '../../models/ticket';
+
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -121,4 +123,29 @@ it("publishes an event", async () => {
     })
     .expect(200);
   expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
+
+it("rejects updates if the ticket is reserved", async () => {
+  const cookie = global.signin()
+  const response = await request(app)
+  .post('/api/tickets')
+  .set('Cookie', cookie)
+  .send({
+    title: 'asldkfj',
+    price: 20,
+  });
+
+  const ticket = await Ticket.findById(response.body.id)
+  ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() })
+
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'updated',
+      price: 1000,
+    })
+    .expect(400);
 })
