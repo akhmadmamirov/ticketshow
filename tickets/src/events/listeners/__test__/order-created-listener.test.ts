7351b540bb64a769de9ea7402a985c86ed930728
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-02-04 14:19:02 -0500

    resolving tests for data replication between services

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -2,7 +2,7 @@ import { OrderCreatedEvent, OrderStatus } from "akhmadillobekcommon";
 import { Ticket } from "../../../models/ticket";
 import { natsWrapper } from "../../../nats-wrapper"
 import { OrderCreatedListener } from "../order-created-listener"
-import mongoose from "mongoose";
+import mongoose, { set } from "mongoose";
 import { Message } from "node-nats-streaming";
 
 const setup = async () => {
@@ -38,3 +38,29 @@ const setup = async () => {
 
   return { listener, data, msg, ticket }
 }
+
+it("sets the userId of the ticket", async () => {
+  const { listener, ticket, data, msg } = await setup()
+
+  await listener.onMessage(data, msg)
+
+  const updatedTicket = await Ticket.findById(ticket.id)
+
+  expect(updatedTicket!.orderId).toEqual(data.id)
+})
+
+it("acks the message", async () => {
+  const { listener, ticket, data, msg } = await setup()
+
+  await listener.onMessage(data, msg)
+
+  expect(msg.ack).toHaveBeenCalled()
+})
+
+it("publishes a ticket updated event", async () => {
+  const { ticket, data, msg, listener } = await setup()
+
+  await listener.onMessage(data, msg)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,6 +3,7 @@ import { OrderCreatedEvent } from "akhmadillobekcommon";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -17,9 +18,18 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     }
     //Mark the ticket being reserved by setting orderId
     ticket.set({ orderId: data.id})
-    
+
     //Save the ticket
     await ticket.save()
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id, 
+      price: ticket.price, 
+      title: ticket.title, 
+      userId: ticket.userId, 
+      orderId: ticket.orderId,
+      version: ticket.version
+    })
 
     //Ack the message
     msg.ack()
