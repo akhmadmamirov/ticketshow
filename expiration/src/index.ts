59d8beeb8c1610a24c7daec18414fb57cab61a9b
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-02-05 08:46:39 -0500

    adding expiration listener

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/order-created-listener';
 
 const start = async () => {
   if (!process.env.NATS_CLIENT_ID) {
@@ -26,7 +27,9 @@ const start = async () => {
     process.on("SIGINT", () => natsWrapper.client.close())
     process.on("SIGTERM", () => natsWrapper.client.close())
 
-    console.log('Connected to MongoDb');
+    new OrderCreatedListener(natsWrapper.client).listen();
+
+
   } catch (err) {
     console.error(err);
   }
