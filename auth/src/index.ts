b9fa65f163276fc213aee001f96db7e641e8e8aa
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-02-10 12:06:55 -0500

    adding starter message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("Ticket...")
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
